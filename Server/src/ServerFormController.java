bfced27207122f15232420d02c67809e8a80cd37
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-08-11 11:24:53 +0530

    'UI'

--- Server/src/ServerFormController.java
+++ Server/src/ServerFormController.java
@@ -1,2 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class ServerFormController {
+    public AnchorPane root;
+    public TextArea txtArea;
+    public TextField txtfield;
+
+    public void send(ActionEvent actionEvent) {
+    }
 }
